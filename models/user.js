29d9af52cdb37f6e39c1d35b60568e3e27957ca7
Alexandre Broquet <a.broquet@worldcastconnect.com>
2019-10-17 11:30:43 +0200

    missing views

--- models/user.js
+++ models/user.js
@@ -1,104 +1,116 @@
-const Redis = require('ioredis');
-const redis = new Redis(process.env.REDIS_PORT || 6379, process.env.REDIS_HOST || '127.0.0.1');
-const table_user = 'user:';
-const uuidv4 = require('uuid/v4');
+const Redis = require("ioredis");
+const redis = new Redis(
+  process.env.REDIS_PORT || 6379,
+  process.env.REDIS_HOST || "127.0.0.1"
+);
+const table_user = "user:";
+const uuidv4 = require("uuid/v4");
 
 module.exports = {
-	get: (userId) => {
-		return (
-			redis
-				// @ts-ignore
-				.hgetall(table_user + userId)
-				.then((resolve) => {
-					return resolve;
-				})
-				.catch((reject) => {
-					console.log(new Error(reject));
-				})
-		);
-	},
+  get: userId => {
+    return (
+      redis
+        // @ts-ignore
+        .hgetall(table_user + userId)
+        .then(resolve => {
+          return resolve;
+        })
+        .catch(reject => {
+          console.log(new Error(reject));
+        })
+    );
+  },
 
-	count: () => {
-		var stream = redis.scanStream({ match: 'user:*', count: 10000 });
-		return new Promise(function(resolve, reject) {
-			stream.on('data', (resultkey) => {
-				console.log('count = ' + resultkey.length);
-				resolve(resultkey.length);
-			});
-		});
-	},
+  count: () => {
+    var stream = redis.scanStream({ match: "user:*", count: 10000 });
+    return new Promise(function(resolve, reject) {
+      stream.on("data", resultkey => {
+        console.log("count = " + resultkey.length);
+        resolve(resultkey.length);
+      });
+    });
+  },
 
-	getAll: (limit, offset) => {
-		// hgetall: Get all the fields and values in a hash
-		let response = [];
-		var stream = redis.scanStream({ match: 'user:*', count: limit === undefined ? 10000 : limit });
+  getAll: (limit, offset) => {
+    // hgetall: Get all the fields and values in a hash
+    let response = [];
+    var stream = redis.scanStream({
+      match: "user:*",
+      count: limit === undefined ? 10000 : limit
+    });
 
-		return new Promise(function(resolve, reject) {
-			stream.on('data', (resultkey) => {
-				Promise.all(
-					resultkey.map(async (element) => {
-						return new Promise(function(resolve, reject) {
-							let UserId = element.split(':')[1];
-							let object1 = { rowid: UserId };
-							resolve(
-								redis.hgetall(table_user + UserId).then((resolve) => {
-									response.push(Object.assign(object1, resolve));
-								})
-							);
-						});
-					})
-				).then((_resovle) => {
-					resolve(response);
-				});
-			});
-		});
-	},
+    return new Promise(function(resolve, reject) {
+      stream.on("data", resultkey => {
+        Promise.all(
+          resultkey.map(async element => {
+            return new Promise(function(resolve, reject) {
+              let UserId = element.split(":")[1];
+              let object1 = { rowid: UserId };
+              resolve(
+                redis.hgetall(table_user + UserId).then(resolve => {
+                  response.push(Object.assign(object1, resolve));
+                })
+              );
+            });
+          })
+        ).then(_resovle => {
+          resolve(response);
+        });
+      });
+    });
+  },
 
-	insert: (params) => {
-		let UserId = uuidv4();
-		console.log(UserId);
-		return redis
-			.hmset(table_user + UserId, params)
-			.then((resolve) => {
-				return resolve;
-			})
-			.catch((reject) => {
-				console.log(new Error(reject));
-			});
-	},
+  insert: params => {
+    let UserId = uuidv4();
+    console.log(UserId);
+    return redis
+      .hmset(table_user + UserId, params)
+      .then(resolve => {
+        return resolve;
+      })
+      .catch(reject => {
+        console.log(new Error(reject));
+      });
+  },
 
-	update: (userId, params) => {
-		const possibleKeys = [ 'firstname', 'lastname', 'email', 'pseudo', 'password' ];
-		console.log('userId = ' + userId);
-		if (
-			Object.keys(params).length === possibleKeys.length &&
-			Object.keys(params).every((element) => {
-				return possibleKeys.includes(element);
-			})
-		) {
-			return redis
-				.hmset(table_user + userId, params)
-				.then((resolve) => {
-					return resolve;
-				})
-				.catch((reject) => {
-					console.log(new Error(reject));
-				});
-		} else {
-			let err = new Error('Bad Request');
-			err.status = 400;
-			return Promise.reject(err);
-		}
-	},
+  update: (userId, params) => {
+    const possibleKeys = [
+      "firstname",
+      "lastname",
+      "email",
+      "pseudo",
+      "password"
+    ];
+    console.log("userId = " + userId);
+    if (
+      Object.keys(params).length === possibleKeys.length &&
+      Object.keys(params).every(element => {
+        return possibleKeys.includes(element);
+      })
+    ) {
+      return redis
+        .hmset(table_user + userId, params)
+        .then(resolve => {
+          return resolve;
+        })
+        .catch(reject => {
+          console.log(new Error(reject));
+        });
+    } else {
+      let err = new Error("Bad Request");
+      err.status = 400;
+      return Promise.reject(err);
+    }
+  },
 
-	remove: (userId) => {
-		return redis
-			.del(table_user + userId)
-			.then((resolve) => {
-				return resolve;
-			})
-			.catch((reject) => {
-				console.log(new Error(reject));
-			});
-	}
+  remove: userId => {
+    return redis
+      .del(table_user + userId)
+      .then(resolve => {
+        return resolve;
+      })
+      .catch(reject => {
+        console.log(new Error(reject));
+      });
+  }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,121 +1,148 @@
-const router = require('express').Router()
-const User = require('../models/user')
+const router = require("express").Router();
+const User = require("../models/user");
 
 /* Users : liste */
-router.get('/', function(req, res, next) {
-  let limit = parseInt(req.query.limit) || 20
-  let offset = parseInt(req.query.offset) || 0
-  if (limit > 100) limit = 100
+router.get("/", function(req, res, next) {
+  let limit = parseInt(req.query.limit) || 20;
+  let offset = parseInt(req.query.offset) || 0;
+  if (limit > 100) limit = 100;
 
-  Promise.all([
-    User.getAll(limit, offset),
-    User.count()
-  ]).then((results) => {
-        res.format({
-      html: () => {
-        res.render('users/index', {
-          users: results[0],
-          count: results[1].count,
-          limit: limit,
-          offset: offset
-        })
-      },
-      json: () => {
-        res.send({
-          data: results[0],
-          meta: {
-            count: results[1].count
-          }
-        })
-      }
+  Promise.all([User.getAll(limit, offset), User.count()])
+    .then(results => {
+      res.format({
+        html: () => {
+          res.render("users/index", {
+            users: results[0],
+            count: results[1].count,
+            limit: limit,
+            offset: offset
+          });
+        },
+        json: () => {
+          res.send({
+            data: results[0],
+            meta: {
+              count: results[1].count
+            }
+          });
+        }
+      });
     })
-  }).catch(next)
-})
+    .catch(next);
+});
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
   if (
-    !req.body.pseudo || req.body.pseudo === '' ||
-    !req.body.email || req.body.email === '' ||
-    !req.body.firstname || req.body.firstname === ''
+    !req.body.pseudo ||
+    req.body.pseudo === "" ||
+    !req.body.email ||
+    req.body.email === "" ||
+    !req.body.firstname ||
+    req.body.firstname === ""
   ) {
-    let err = new Error('Bad Request')
-    err.status = 400
-    return next(err)
+    let err = new Error("Bad Request");
+    err.status = 400;
+    return next(err);
   }
 
-  User.insert(req.body).then(() => {
-    res.format({
-      html: () => { res.redirect('/users') },
-      json: () => { res.status(201).send({message: 'success'}) }
+  User.insert(req.body)
+    .then(() => {
+      res.format({
+        html: () => {
+          res.redirect("/users");
+        },
+        json: () => {
+          res.status(201).send({ message: "success" });
+        }
+      });
     })
-  }).catch(next)
-})
+    .catch(next);
+});
 
-
-router.get('/add', function(req, res, next) {
+router.get("/add", function(req, res, next) {
   res.format({
     html: () => {
-      res.render('users/edit', {
+      res.render("users/edit", {
         user: {},
-        action: '/users'
-      })
+        action: "/users"
+      });
     },
     json: () => {
-      let error = new Error('Bad Request')
-      error.status = 400
-      next(error)
+      let error = new Error("Bad Request");
+      error.status = 400;
+      next(error);
     }
-  })
-})
+  });
+});
 
-router.get('/:userId/edit', function(req, res, next) {
-  User.get(req.params.userId).then((user) => {
-    if (!user) return next()
+router.get("/:userId/edit", function(req, res, next) {
+  User.get(req.params.userId)
+    .then(user => {
+      if (!user) return next();
 
-    res.format({
-      html: () => {
-        res.render('users/edit', {
-          user: user,
-          action: `/users/${req.params.userId}?_method=put`
-        })
-      },
-      json: () => {
-        let error = new Error('Bad Request')
-        error.status = 400
-        next(error)
-      }
+      res.format({
+        html: () => {
+          res.render("users/edit", {
+            user: user,
+            action: `/users/${req.params.userId}?_method=put`
+          });
+        },
+        json: () => {
+          let error = new Error("Bad Request");
+          error.status = 400;
+          next(error);
+        }
+      });
     })
-  }).catch(next)
-})
+    .catch(next);
+});
 
-router.get('/:userId', (req, res, next) => {
-  User.get(req.params.userId).then((user) => {
-    if (!user) return next()
+router.get("/:userId", (req, res, next) => {
+  User.get(req.params.userId)
+    .then(user => {
+      if (!user) return next();
 
-    res.format({
-      html: () => { res.render('users/show', { user: user }) },
-      json: () => { res.send({ data: user }) }
+      res.format({
+        html: () => {
+          res.render("users/show", { user: user });
+        },
+        json: () => {
+          res.send({ data: user });
+        }
+      });
     })
-  }).catch(next)
-})
+    .catch(next);
+});
 
-router.put('/:userId', (req, res, next) => {
+router.put("/:userId", (req, res, next) => {
   console.log(JSON.stringify("request params = " + JSON.stringify(req.params)));
-  User.update(req.params.userId, req.body).then(() => {
-    res.format({
-      html: () => { res.redirect(`/users/${req.params.userId}`) },
-      json: () => { res.status(200).send({ message: 'success' }) }
+  User.update(req.params.userId, req.body)
+    .then(() => {
+      res.format({
+        html: () => {
+          res.redirect(`/users/${req.params.userId}`);
+        },
+        json: () => {
+          res.status(200).send({ message: "success" });
+        }
+      });
     })
-  }).catch(next)
-})
+    .catch(next);
+});
 
-router.delete('/:userId', (req, res, next) => {
-  User.remove(req.params.userId).then(() => {
-    res.format({
-      html: () => { res.redirect(`/users`) },
-      json: () => { res.status(200).send({ message: 'success' }) }
+router.delete("/:userId", (req, res, next) => {
+  User.remove(req.params.userId)
+    .then(() => {
+      res.format({
+        html: () => {
+          res.redirect(`/users`);
+        },
+        json: () => {
+          res.status(200).send({ message: "success" });
+        }
+      });
     })
-  }).catch(next)
-})
+    .catch(next);
+});
 
-module.exports = router
+module.exports = router;
